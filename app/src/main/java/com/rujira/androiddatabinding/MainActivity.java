62112b4de6d6ef44acf3913dc86fc14efead40bd
Rujira Kongsomran <biwberry@outlook.com>
2020-01-01 21:28:48 +0700

    Binding between UI and Model

--- app/src/main/java/com/rujira/androiddatabinding/MainActivity.java
+++ app/src/main/java/com/rujira/androiddatabinding/MainActivity.java
@@ -1,8 +1,12 @@
 package com.rujira.androiddatabinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.rujira.androiddatabinding.model.User;
+import com.rujira.androiddatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User defaultUser = new User();
+        defaultUser.setFirstName("Rujira");
+        defaultUser.setLastName("Kongsomran");
+        binding.setUser(defaultUser);
     }
 }
--- app/src/main/java/com/rujira/androiddatabinding/model/User.java
+++ app/src/main/java/com/rujira/androiddatabinding/model/User.java
@@ -1,4 +1,4 @@
-package com.rujira.androiddatabinding.Model;
+package com.rujira.androiddatabinding.model;
 
 public class User {
     private String firstName;
